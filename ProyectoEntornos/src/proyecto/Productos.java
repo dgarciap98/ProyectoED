8e8b4b9000453f1fec29af440b7b4520a6b0c9c7
davis <davis@Portatil_David.mshome.net>
2022-05-13 10:38:19 +0200

    Clase tienda creada

--- ProyectoEntornos/src/proyecto/Productos.java
+++ ProyectoEntornos/src/proyecto/Productos.java
@@ -5,17 +5,19 @@ public class Productos {
 	protected int lote;
 	protected String nombre;
 	protected String tipo;
-	
+	protected int stock;
 	public Productos() {
 		lote = 0;
 		nombre = "";
 		tipo = "";
+		stock = 0;
 	}
 	
-	public Productos(int lote, String nombre, String tipo) {
+	public Productos(int lote, String nombre, String tipo, int stock) {
 		this.lote = lote;
 		this.nombre = nombre;
 		this.tipo = tipo;
+		this.stock = stock;
 	}
 
 	public int getLote() {
@@ -42,9 +44,17 @@ public class Productos {
 		this.tipo = tipo;
 	}
 	
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
 	public String toString() {
 		
-		return "Producto: " + getNombre() + "\t Tipo: " + getTipo() + "\t Lote: " + getLote();
+		return "Producto: " + getNombre() + "\t Tipo: " + getTipo() + "\t Stock: " + getStock() + "\t Lote: " + getLote();
 	}
 
 }
--- ProyectoEntornos/src/proyecto/TIenda.java
+++ ProyectoEntornos/src/proyecto/TIenda.java
@@ -1,10 +1,70 @@
 package proyecto;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class TIenda {
 
 	public static void main(String[] args) {
 		
+		FileReader fr = null;
+		BufferedReader br = null;
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		ArrayList<Productos> listaProductos = new ArrayList<Productos>();
+		String linea = "";
 
+		try {
+			fr = new FileReader("C:\\test\\productos.csv");
+			br = new BufferedReader(fr);
+			fw = new FileWriter("C:\\test\\listado.csv");
+			bw = new BufferedWriter(fw);
+			
+			while(br.ready()) {
+				
+				linea = br.readLine();
+				
+				// Aqui introducir las listas
+				
+				
+				// Aqui introducir en la listaProductos el contenido de las listas
+												
+			}
+			
+			// Para quitar primera linea del fichero
+			bw.newLine();
+			
+			for (Productos l : listaProductos) {
+				System.out.println(l);
+				
+			}
+			
+		} catch (FileNotFoundException ex) {
+			System.out.println("Fichero no encontrado");
+		} catch (IOException ex) {
+			System.out.println("Error de lectura/escritura");
+
+		} finally {
+			try {
+				if(bw != null) {
+					bw.close();
+				}
+				if(fw != null) {
+					fw.close();
+				}
+				if(br != null) {
+					br.close();
+				}
+				if(fr != null) {
+					fr.close();
+				}
+				
+			} catch (IOException ex) {
+				System.out.println("Error al cerrar escritor/lector");
+			}
+			
+		}
+		
 	}
 
 }
